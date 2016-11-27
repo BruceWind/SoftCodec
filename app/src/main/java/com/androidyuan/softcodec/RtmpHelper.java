38badd27023bf0def88e0f9b39385e1beffc6c13
weizongwei5 <weizongwei5@gmail.com>
2016-11-27 20:08:42 +0800

    打印FPS

--- app/src/main/java/com/androidyuan/softcodec/RtmpHelper.java
+++ app/src/main/java/com/androidyuan/softcodec/RtmpHelper.java
@@ -1,13 +1,18 @@
 package com.androidyuan.softcodec;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+import android.util.Log;
 /**
  * Created by wei on 16-11-27.
  */
 
 public class RtmpHelper {
+
+    Timer timer;
+    long fps=0;
 
 
     final ExecutorService rtmpExecutor = Executors.newSingleThreadExecutor();
@@ -16,10 +21,23 @@ public class RtmpHelper {
 
     /*rtmp*/
     public  int rtmpOpen(final String url){
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("RtmpHelper","fps = "+fps);
+                fps=0;
+            }
+        },1000,1000);
+
         return mStreamHelper.rtmpOpen(url);
     }
 
     public  int rtmpStop(){
+        if(timer!=null) {
+            timer.cancel();
+        }
+
         rtmpExecutor.execute(new Runnable() {
             @Override
             public void run() {
@@ -35,6 +53,7 @@ public class RtmpHelper {
         rtmpExecutor.execute(new Runnable() {
             @Override
             public void run() {
+                fps++;
                 mStreamHelper.compressBuffer(encoder,NV12,NV12size,H264);
             }
         });
@@ -44,7 +63,6 @@ public class RtmpHelper {
     public  long compressBegin(final int width, int height, int bitrate, int fps)
     {
         return  mStreamHelper.compressBegin(width,height,bitrate,fps);
-
     }
 
     public  int compressEnd(long encoder)
